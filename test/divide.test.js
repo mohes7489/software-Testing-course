2c78329ba608426e68799352b16450226fcab564
hmmoal <mohammed.al-gburi@tuni.fi>
2024-11-21 00:05:10 +0200

    adding divide test cases

--- test/divide.test.js
+++ test/divide.test.js
@@ -1,8 +1,8 @@
-/* import divide from "../src/divide";
+import divide from "../src/divide.js";
 import { expect } from "chai";
 
 describe("divide", function() {
-    it("", function() {
-        
+    it("should return 1.5 when divided 6 over 4 ", function() {
+        expect(divide(6,4)).to.equal(1.5);
     });
-}) */
+})
