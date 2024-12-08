15726807994a07f7d79f8b5028833128d14ac122
Ioannis Panagiotopoulos <jpanagiotopoulos@crowdpolicy.com>
2024-12-09 01:56:16 +0200

    new test_case commit

--- src/countBy.js
+++ src/countBy.js
@@ -39,3 +39,10 @@ function countBy(collection, iteratee) {
 }
 
 export default countBy
+
+
+  [
+    { 'user': 'barney', 'active': true },
+    { 'user': 'betty', 'active': true },
+    { 'user': 'fred', 'active': false }
+  ]
